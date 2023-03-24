dc5c052fe6af3bccf6cd2b1cd636814ffa94feb1
Jeffrey Le <jeffreyle20@gmail.com>
2023-03-24 17:34:54 -0600

    Created test function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +17,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::test() {
+    int x = 0;
+    x++;
+    x++;
+
+    std::string y = "";
+
+    cout << x << y << endl;
+    return;
+}
