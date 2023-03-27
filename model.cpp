a97f456c10b4324f4e21c82ee7d542acfd477c39
jeffohh <jeffreyle20@gmail.com>
2023-03-26 18:27:33 -0600

    Implemented barebone Mouse events
    - canvas initalization
    - canvas view update communication

--- model.cpp
+++ model.cpp
@@ -2,6 +2,37 @@
 
 Model::Model(QObject *parent)
     : QObject{parent}
+    , canvas(32, 32, QImage::Format_RGB32)
 {
+    canvas.fill(Qt::red);
+
+    emit updateCanvas(&canvas);
+}
+
+
+void Model::mouseDown(QPoint pos) {
+
+    switch (tool) {
+
+    case PENCIL:
+        // set pixel at location
+        canvas.setPixelColor(pos, toolColor);
+
+        // update view
+        emit updateCanvas(&canvas);
+
+        break;
+
+    default:
+        break;
+
+    }
+}
+
+void Model::mouseMove(QPoint) {
+
+}
+
+void Model::mouseUp(QPoint) {
 
 }
